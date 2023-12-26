6a58e32a0263a1442c59f4ec7eb7bc4c3522d3d4
Roberto <roberto.v.arebalo@gmail.com>
2023-12-26 20:01:23 -0300

    agrego la libreria de bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  return(<h1>Prueba</h1>)
+  return(<h1 className='text-center  display-3'>
+    {}
+    Prueba</h1>)
 }
 
 export default App
