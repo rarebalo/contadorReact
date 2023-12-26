a096f51ac6fe02b9d43959da9afca1db7288cd61
Roberto <roberto.v.arebalo@gmail.com>
2023-12-26 20:08:24 -0300

    agrego el maquetado del componente app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return(<h1 className='text-center  display-3'>
-    {}
-    Prueba</h1>)
+  return (
+    <section className="container my-4">
+      <h1 className="text-center  display-3">
+        {}
+        Contador
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos est
+        natus repellat veniam sit, atque expedita aliquam ipsam voluptatum
+        exercitationem aperiam quasi. Officiis nobis molestias labore neque
+        corporis harum debitis!
+      </p>
+    </section>
+  );
 }
 
-export default App
+export default App;
